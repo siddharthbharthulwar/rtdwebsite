b4c4c56ab28298dc8f158e9939afe6e8fbe785e1
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2020-06-29 14:14:58 -0600

    add feedpoller js

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 var realtime = require('rtd-realtime');
 
-realtime.VehiclePositions.write( (err,feed) => {
+function Position(latitude, longitude, bearing) {
+
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.bearing = bearing;
+
+}
+
+function BusInstance(trip_id, route_id, direction_id, Position){
+
+    this.trip_id = trip_id;
+    this.route_id = route_id;
+    this.direction_id = direction_id;
+    this.Position = Position;
+
+}
+
+realtime.VehiclePositions.load( (err,feed) => {
     // 'err' will be supplied if an error occured in the request or decoding of the feed,
     // otherwise 'err' will be null
     // 'feed' will be supplied as decode json representation of a GTFS-RT feed
-    // A JSON file of the feed will be written to your file system
+    var entity = feed['entity'];
+    var instances = [];
+
+    for (var i = 0; i < entity.length; i++){
+
+        var obj = entity[i];
+
+        if ( typeof obj.vehicle.trip !== 'undefined' && obj.vehicle.trip ){
+
+            console.log(i);
+            console.log(obj);
+    
+            var pos = Position(obj.vehicle.trip.latitude, obj.vehicle.trip.longitude, obj.vehicle.trip.bearing);
+            var bus = BusInstance(obj.vehicle.trip.trip_id, obj.vehicle.trip.route_id, obj.vehicle.trip.direction_id, pos);
+            instances.push(bus);
+
+        }
+
+    }
+
+    console.log(instances);
 });
